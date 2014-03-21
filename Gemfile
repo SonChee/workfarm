39863f40d3db2e6c2c8f26bcba3f7ae8a71b6db4
Son Che <che.dinh.son@framgia.com>
2014-03-21 20:26:47 +0700

    finish create user

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.0'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem "sass-rails", "~> 4.0.0"
+gem 'bcrypt-ruby', '3.1.2'
 gem "mysql2"
 group :development, :test do
   gem 'rspec-rails', '2.13.1'
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,30 @@
 class Admin::UsersController < ApplicationController
-  def new
+  def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to admin_user_path @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:code, :name, :email, :password,
+                                   :password_confirmation)
+    end
 end
