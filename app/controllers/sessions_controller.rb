d1087ff54041d8f2a35f276b3cbf1f79bcc49a7c
Son Che <che.dinh.son@framgia.com>
2014-07-17 08:07:31 +0700

    bug pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #TODO authenticate and redirect
